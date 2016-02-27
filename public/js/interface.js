1e0c28188cd4255a212a5b68bf90aa40833d461e
blancopado <pablo.dominguezblanco@gmail.com>
2016-02-27 22:36:06 +0100

    openWeatherMap API call

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,7 +1,14 @@
-
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemp();
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',sp&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp + ' C')
+    });
+  });
+
 
   $('#power_saving_mode_on').css('background-color', 'green');
   $('#power_saving_mode_off').css('background-color', 'white').fadeTo("slow", 0.15);
@@ -37,6 +44,7 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature());
     $('#temperature').attr('class', thermostat.energyConsumption());
   };
+
 });
 
 $("body").hide().fadeIn(2500);
